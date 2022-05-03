20b4c233671168637e449a6692778318d73e8a41
Jean-Pierre Both <jeanpierre.both@gmail.com>
2022-05-03 09:03:10 +0200

    loading in hnsw runs

--- src/bin/bigann.rs
+++ src/bin/bigann.rs
@@ -60,10 +60,13 @@ fn read_data_block<const SIZE: usize>(data_buf : &mut BufReader<File>, nb_data: 
 
 
 // read ground truth format
-fn read_ground_truth(gt_fname : String) -> Result< Vec<Vec<(u32, f32)>>, anyhow::Error> {
+fn read_ground_truth(path : PathBuf) -> Result< Vec<Vec<(u32, f32)>>, anyhow::Error> {
     // read nbquery(uint32 little endian)
-    let path = PathBuf::from(gt_fname);
-    let gt_file = OpenOptions::new().read(true).open(&path).unwrap();
+    let gt_file = OpenOptions::new().read(true).open(&path);
+    if gt_file.is_err() {
+        log::error!("could not open file : {}", path.display());
+    }
+    let gt_file = gt_file.unwrap();
     let mut gt_buf = BufReader::new(gt_file);
     let nb_query : usize = gt_buf.read_u32::<LittleEndian>().unwrap() as usize;
     let knn : usize = gt_buf.read_u32::<LittleEndian>().unwrap() as usize; // get number of neighbours
@@ -104,10 +107,13 @@ fn read_ground_truth(gt_fname : String) -> Result< Vec<Vec<(u32, f32)>>, anyhow:
 
 
 // read query vector
-fn read_query(query_fname : String) -> Result< Vec<Vec<u8>>, anyhow::Error> {
+fn read_query(path : PathBuf) -> Result< Vec<Vec<u8>>, anyhow::Error> {
     //
-    let path = PathBuf::from(query_fname);
-    let query_file = OpenOptions::new().read(true).open(&path).unwrap();
+    let query_file = OpenOptions::new().read(true).open(&path);
+    if query_file.is_err() {
+        log::error!("read_query , could not open file : {}", path.display());
+    }
+    let query_file = query_file.unwrap();
     let mut query_buf = BufReader::new(query_file);
     let nb_data : u32 = query_buf.read_u32::<LittleEndian>().unwrap();
     let nb_data = nb_data as usize;
@@ -130,14 +136,14 @@ pub fn main() {
     //
     let dirname = String::from(BIGANN_DIR);
     //
-    let mut data_fname = dirname.clone();
-    data_fname.push_str("/learn.100M.u8bin");
+    let mut data_fname = PathBuf::from(dirname.clone());
+    data_fname.push("learn.100M.u8bin");
     //
-    let mut query_fname = dirname.clone();
-    query_fname.push_str("query.public.10K.u8bin");
+    let mut query_fname = PathBuf::from(dirname.clone());
+    query_fname.push("query.public.10K.u8bin");
     //
-    let mut ground_truth_fname = dirname.clone();
-    ground_truth_fname.push_str("/public_query_gt100.bin");
+    let mut ground_truth_fname = PathBuf::from(dirname.clone());
+    ground_truth_fname.push("public_query_gt100.bin");
     //
     let path = PathBuf::from(data_fname);
     let data_file_res = OpenOptions::new().read(true).open(&path);
@@ -155,8 +161,8 @@ pub fn main() {
         std::panic!("expected dim {}, got {}", DIM, dim);
     }
     // we will read data by block doing parallel insertion in Hnsw, read_data_block running async
-    let ef_c = 50;
-    let max_nb_connection = 70;
+    let ef_c = 64;
+    let max_nb_connection = 100;
     let nb_layer = 16.min((nb_data as f32).ln().trunc() as usize);
     let block : usize = 10000;
     //
@@ -165,6 +171,7 @@ pub fn main() {
     let hnsw = Hnsw::<u8, DistL2>::new(max_nb_connection, nb_data, nb_layer, ef_c, DistL2{});
     // now we loop reading inserting
     let mut nb_data_read = 0;
+    let test = false;
     loop {
         let new_datas = read_data_block::<DIM>(&mut data_buf, block);
         if new_datas.is_err() {
@@ -177,8 +184,13 @@ pub fn main() {
         }
         // insert in Hnsw
         let data_with_id : Vec<(&Vec<u8>, DataId)>= new_data.iter().zip(0..new_data.len()).map(|v| (v.0, v.1 + nb_data_read)).collect();
-//        hnsw.parallel_insert(&data_with_id);
+        if !test {
+            hnsw.parallel_insert(&data_with_id);
+        }
         nb_data_read += new_data.len();
+        if !test {
+            nb_data_read += new_data.len();
+        }
         if nb_data_read == nb_data {
             log::info!("exiting loop nb data read : {}", nb_data_read);
             break;
@@ -187,12 +199,19 @@ pub fn main() {
     let cpu_time: Duration = cpu_start.elapsed();
     println!(" ann construction sys time(s) {:?} cpu time {:?}", sys_now.elapsed().unwrap().as_secs(), cpu_time.as_secs());
     hnsw.dump_layer_info();
+    // dump in a file. Must take care of name as tests runs in // !!!
+    if test {
+        let fname = String::from("dumpbigann");
+        log::info!("dumping in files : {} ... ", fname);
+        let _res = hnsw.file_dump(&fname); 
+        log::info!("dump finished");
+    }   
     //
     // load ground truth to know how many neighbours we must search
     //
     let gtruth = read_ground_truth(ground_truth_fname.clone());
     if gtruth.is_err() {
-        log::error!("error opening ground truth file : {}, err : {:?}", ground_truth_fname, gtruth.as_ref().err().unwrap());
+        log::error!("error opening ground truth file : {}, err : {:?}", ground_truth_fname.display(), gtruth.as_ref().err().unwrap());
     }
     let gtruth = gtruth.unwrap();
     //
